7eaadffb60bd4aa985cd2520c36b024af8c9c1e7
ionutspalatelu@gmail.com <ionutspalatelu@gmail.com>
2019-06-12 15:04:40 +0300

    Verified homework 1

--- src/a_25_05/ex5.java
+++ src/a_25_05/ex5.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class ex5 {
     public static void main(String[] args) {
+        //variabilele incep cu litera mica
         int[] Primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
         Scanner numar = new Scanner(System.in);
         System.out.println("Introduce numarul");
