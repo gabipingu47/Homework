358bfbb4867e8495efad67f78a9ba9ab1be8a2e9
ionutspalatelu@gmail.com <ionutspalatelu@gmail.com>
2019-08-02 21:06:40 +0300

    Verified assignment
    Signed-off-by: Ionut Spalatelu <ionut.spalatelu@gohenry.co.uk>

--- src/main/java/assignment/ShapeFeatures.java
+++ src/main/java/assignment/ShapeFeatures.java
@@ -1,9 +1,9 @@
 package assignment;
 
-import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import lombok.Data;
 
 @Data
 
@@ -15,39 +15,50 @@ public class ShapeFeatures extends Shape {
         shapes.add(shape);
     }
 
+    //interesanta abordarea cu switch-ul, dar el face exact ce ar fi facut polimorfismul pt tine
     public double calcPerimeter(Shape shape) {
-        switch (Shape shape) {
+        //aici mai aveai nevoie de asta
+        String shapeType = shape.getClass().getSimpleName().toLowerCase();
+
+        double perimeter = 0;
+        switch (shapeType) {
             case "triangle":
-                System.out.println("Triangle perimeter is " + Triangle.side1 + Triangle.side2 + Shape.base);
+                perimeter = Triangle.side1 + Triangle.side2 + Shape.base;
+                System.out.println("Triangle perimeter is " + perimeter);
                 break;
             case "square":
-                System.out.println("Square perimeter is " + Shape.base*4);
+                perimeter = Shape.base * 4;
+                System.out.println("Square perimeter is " + perimeter);
                 break;
             case "rectangle":
-                System.out.println("Rectangle perimeter is " + Shape.base*2 + Shape.height*2);
+                System.out.println("Rectangle perimeter is " + Shape.base * 2 + Shape.height * 2);
                 break;
             case "pentagon":
-                System.out.println("Pentagon perimeter is " + Shape.base*5);
+                System.out.println("Pentagon perimeter is " + Shape.base * 5);
                 break;
             default:
-            System.out.println("Please specify a valid shape!");
+                System.out.println("Please specify a valid shape!");
+        }
     }
 
-    public double calcArea (Shape shape) {
+    public double calcArea(Shape shape) {
+        String shapeType = shape.getClass().getSimpleName().toLowerCase();
+        switch (shapeType) {
             case "triangle":
-                System.out.println("Triangle area is " + (Shape.base*Shape.height)/2);
+                System.out.println("Triangle area is " + (Shape.base * Shape.height) / 2);
                 break;
             case "square":
                 System.out.println("Square area is " + Math.pow(Shape.base, 2);
                 break;
             case "rectangle":
-                System.out.println("Rectangle area is " + Shape.base*2 + Shape.height*2);
+                System.out.println("Rectangle area is " + Shape.base * 2 + Shape.height * 2);
                 break;
             case "pentagon":
-                System.out.println("Pentagon perimeter is " + (((Shape.base*5)*Shape.height)/2);
+                System.out.println("Pentagon perimeter is " + (((Shape.base * 5) * Shape.height) / 2);
                 break;
             default:
                 System.out.println("Please specify a valid shape!");
         }
 
+    }
 }
--- src/main/java/assignment/Triangle.java
+++ src/main/java/assignment/Triangle.java
@@ -1,7 +1,5 @@
 package assignment;
 
-import assignment.Shape;
-
 public class Triangle extends Shape {
     public static double side1;
     public static double side2;
